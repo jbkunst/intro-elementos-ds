6fdad047a9c05b5d2a674aba3145121df8500c24
Joshua Kunst <jbkunst@gmail.com>
2022-07-07 19:00:25 -0400

    Update 99-spotify-exp.R

--- R/99-spotify-exp.R
+++ R/99-spotify-exp.R
@@ -7,18 +7,18 @@ library(spotifyr)
 # http://meetingwords.com/GV7CCr7oKR
 
 data <- "usuario playlist
-jbkunst Top
+nkmeln0t0go2m2vc4gx5ldr27 duchita
+silvasaavedrai2013 80's
+luke.rutherford.1999 TWICE/EG/RV/Anime
+4a1v71wm05ynhpbytzk33mf7j Shi 
+Astorgato AA
 jbkunst SocialNetworks
-lrdsmella duchita
-ISS® Sunshine ♥
-electronmaxi TWICE/EG/RV/Anime
-Taliric Shi 
-Astorgato Uwu"
+9ijft8lahgcenze5pyol0dqgv motivación"
 
 data
 
 data <- read_delim(data)
-
+data <- mutate(data, playlist = str_trim(playlist))
 data
 
 Sys.setenv(SPOTIFY_CLIENT_ID = '5e97191efa69427f9c7f59a0d8a2bebf')
@@ -26,14 +26,25 @@ Sys.setenv(SPOTIFY_CLIENT_SECRET = '0a216fdb384b4a3a84f5d0857627a0b2')
 
 access_token <- get_spotify_access_token()
 
+get_spotify_authorization_code(
+  client_id = Sys.getenv("SPOTIFY_CLIENT_ID"),
+  client_secret = Sys.getenv("SPOTIFY_CLIENT_SECRET"),
+  scope = scopes()
+)
+
 
 # test --------------------------------------------------------------------
-playlist_id <- get_user_playlists(user_id = "jbkunst") |>
+get_user_playlists(user_id = "Astorgato") %>% glimpse()
+get_user_playlists(user_id = "nkmeln0t0go2m2vc4gx5ldr27")
+get_user_playlists(user_id = "9ijft8lahgcenze5pyol0dqgv") %>% glimpse()
+
+
+playlist_id <- get_user_playlists(user_id = "nkmeln0t0go2m2vc4gx5ldr27") |>
   glimpse() |> 
-  filter(name == "SocialNetworks") |> 
+  filter(name == "duchita") |> 
   glimpse() |> 
   pull(id)
-  
+
 playlist_id
 
 
@@ -51,7 +62,6 @@ pl_tracks$track.id
 
 pl_tracks$track.album.release_date
 
-
 features <- get_track_audio_features(pl_tracks$track.id)
 
 # https://developer.spotify.com/documentation/web-api/reference/#/operations/get-several-audio-features
@@ -60,7 +70,78 @@ glimpse(features)
 
 
 # iterar con map ----------------------------------------------------------
+x <- c(1, 4, 56, 99)
+
+exp(x)
+
+for(i in 1:4){
+  print(exp(x[i]))
+}
+
+map(x, exp)
+
+datos <- pmap(data, function(usuario = "Astorgato", playlist = "AA"){
+  
+  message("voy a descargar ", usuario)
+  
+  playlist_id <- get_user_playlists(user_id = usuario) |>
+    filter(name == playlist) |> 
+    pull(id)
+  
+  pl_tracks <- get_playlist_tracks(playlist_id)
+  
+  dim(pl_tracks)
+  
+  features <- get_track_audio_features(pl_tracks$track.id)
+  
+  d <- features %>% 
+    mutate(usuario = usuario, .before = 1) %>% 
+    mutate(
+      anio = as.numeric(str_extract(pl_tracks$track.album.release_date, "[0-9]{4}")),
+      .after = 1
+      )
+  
+  d
+  
+})
 
 
+datos <- bind_rows(datos)
+
+count(datos, usuario)
+
+
+datos %>% 
+  select(where(is.numeric))
+
+
+# umap --------------------------------------------------------------------
+datosnum <- datos %>% 
+  select(where(is.numeric))
+
+library(umap)
+
+dumap <- umap(datosnum)
+
+dumapdf <- dumap$layout %>% 
+  as.data.frame()
+
+datos <- bind_cols(datos, dumapdf)
+
+glimpse(datos)
+
+datos %>% count(usuario)
+
+ggplot(datos) +
+  geom_point(aes(V1, V2, color = usuario)) +
+  scale_color_viridis_d()
+
+
+ggplot(datos) +
+  geom_point(aes(V1, V2), color = "gray90", data = dumapdf) +
+  geom_point(aes(V1, V2, color = tempo)) +
+  facet_wrap(vars(usuario)) +
+  scale_color_viridis_c() +
+  theme_minimal()
 
 
